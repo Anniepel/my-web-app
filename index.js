1bcc7033fba9438f981e83760e3d0ed0eddfea88
Annie Pelouat <ituzepelouat@gmail.com>
2025-12-04 10:04:58 +0100

    Add user authentication feature

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-  res.end("Hello, this is a basic web app!");
+  if (req.url === '/about') {
+    res.end("About Page - This project belongs to Anniepel.");
+  } else {
+    res.end("Hello, this is a basic web app! By Annie Pelouat");
+  }
 });
 
 server.listen(3000, () => {
