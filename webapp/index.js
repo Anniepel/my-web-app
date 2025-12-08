78e2161744e68c746ef13c46ae280a04d339ebb5
Annie Pelouat <ituzepelouat@gmail.com>
2025-12-08 10:27:11 +0100

    Fix: format index.js indentation for /about route

--- webapp/index.js
+++ webapp/index.js
@@ -1,19 +1,18 @@
 const http = require('http');
-const auth = require('./auth');
-
 const server = http.createServer((req, res) => {
+
   if (req.url === '/about') {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
     res.end('About Page\n');
-  } else if (req.url === '/auth') {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end(auth.getWelcomeMessage());
+
   } else {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Hello, this is a basic web app! by Annie Pelouat');
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello, World!\n');
   }
 });
 
 server.listen(3000, () => {
-  console.log('Server running at http://localhost:3000');
+  console.log('Server running at http://localhost:3000/');
 });
